6edcc66aa333feefa2281d98d13a589eac8384bc
Pandananana <andreaswendelboe.dk@gmail.com>
2025-02-23 20:00:24 +0100

    Ex3 done

---
+++ ex3/q2_2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#BSUB -J q2
+#BSUB -q hpc
+#BSUB -W 2
+#BSUB -o logs/q1_2_%J.out
+#BSUB -e logs/q1_2_%J.err
+#BSUB -R "span[hosts=1] rusage[mem=16GB]"
+#BSUB -R "select[model == XeonGold6226R]"
+#BSUB -n 1
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+echo "Running q2_1.py with 256"
+python q2_1.py 256
+
+echo "Running q2_1.py with 512"
+python q2_1.py 512
+
+echo "Running q2_1.py with 1024"
+python q2_1.py 1024
---
+++ ex3/q2_4.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import blosc
+import numpy as np
+from time import perf_counter
+
+
+def write_numpy(arr, file_name):
+    np.save(f"{file_name}.npy", arr)
+    os.sync()
+
+
+def write_blosc(arr, file_name, cname="lz4"):
+    b_arr = blosc.pack_array(arr, cname=cname)
+    with open(f"{file_name}.bl", "wb") as w:
+        w.write(b_arr)
+    os.sync()
+
+
+def read_numpy(file_name):
+    return np.load(f"{file_name}.npy")
+
+
+def read_blosc(file_name):
+    with open(f"{file_name}.bl", "rb") as r:
+        b_arr = r.read()
+    return blosc.unpack_array(b_arr)
+
+
+n = int(sys.argv[1])
+
+mat = np.random.randint(0, 256, size=(n,n,n))
+
+file_name = "q2_3"
+
+t1 = perf_counter()
+write_numpy(mat, file_name)
+t2 = perf_counter()
+print("Write numpy: ", t2-t1)
+
+t1 = perf_counter()
+write_blosc(mat, file_name)
+t2 = perf_counter()
+print("Write blosc: ", t2-t1)
+
+t1 = perf_counter()
+read_numpy(file_name)
+t2 = perf_counter()
+print("Read numpy: ", t2-t1)
+
+t1 = perf_counter()
+read_blosc(file_name)
+t2 = perf_counter()
+print("Read blosc: ", t2-t1)
